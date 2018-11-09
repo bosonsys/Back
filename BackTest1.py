d21e547ae012ce860c620958520b1bad68689630
Muthu <v.muthamizh@gmail.com>
2018-11-09 23:58:39 +0530

    swing added

--- BackTest1.py
+++ BackTest1.py
@@ -24,13 +24,13 @@ print(df.head())
 # With Pands Rolling Function with Percentage
 plt.rcParams["figure.figsize"] = (20,8)
 fig = plt.figure()
-ax1 = plt.subplot2grid((6,1), (0,0), colspan=1, rowspan=6)
-# ax2 = plt.subplot2grid((6,1), (3,0), colspan=1, sharex=ax1)
-# ax3 = plt.subplot2grid((6,1), (4,0), colspan=1, sharex=ax1)
-# ax4 = plt.subplot2grid((6,1), (5,0), colspan=1, sharex=ax1)
+ax1 = plt.subplot2grid((6,1), (0,0), colspan=1, rowspan=3)
+ax2 = plt.subplot2grid((6,1), (3,0), colspan=1, sharex=ax1)
+ax3 = plt.subplot2grid((6,1), (4,0), colspan=1, sharex=ax1)
+ax4 = plt.subplot2grid((6,1), (5,0), colspan=1, sharex=ax1)
 
 def animate(i):
-    name = 'INFIBEAM'
+    name = 'PCJEWELLER'
     query = 'SELECT * FROM kite_watch where tradingsymbol ="'+name+'" and insert_on > "' + gDate + '" and insert_on < "' + nDate + '"'
     df = pd.read_sql_query(query, engine)
     indQ = 'SELECT * FROM indicators where tradingsymbol ="'+name+'" and  insert_on > "' + gDate + '" and insert_on < "' + nDate + '"'
@@ -47,36 +47,35 @@ def animate(i):
     # ax1.plot(indVal['insert_on'], indVal['sma1'])
     # ax1.plot(indVal['insert_on'], indVal['sma2'])
     # ax1.plot(df['insert_on'], df['lastPrice'].rolling(9).mean(), label= 'MA 9 days')
-    # ax1.plot(df['insert_on'], df['lastPrice'].rolling(42).mean(),label= 'MA 42 days')
-    # ax1.plot(df['insert_on'], df['lastPrice'].rolling(100).mean(),label= 'MA 50 days')
-    for sw in swVal.iterrows():
+    # ax1.plot(df['insert_on'], df['lastPrice'].rolling(42).mean(), label= 'MA 42 days')
+    # ax1.plot(df['insert_on'], df['lastPrice'].rolling(100).mean(), label= 'MA 50 days')
+    # for sw in swVal.iterrows():
         # print()
-        ax1.plot([sw[1]['stime'], sw[1]['etime']], [sw[1]['sprice'], sw[1]['eprice']], color="red")
-        ax1.annotate(sw[1]['sLenth'], (sw[1]['etime'], (sw[1]['sprice'])), textcoords='data')
+        # ax1.plot([sw[1]['stime'], sw[1]['etime']], [sw[1]['sprice'], sw[1]['eprice']], color="red")
+        # ax1.annotate(sw[1]['sLenth'], (sw[1]['etime'], (sw[1]['sprice'])), textcoords='data')
 
     # plt.xticks(rotation=45)
     for index, row in callF.iterrows():
             if row['call'] == 1:
                 ax1.annotate('Buy', (row['inserted_on'], (row['price'])), textcoords='data')
-                if row['status'] !=0:
+                if row['status'] != 0:
                     if row['status'] == 1:
                         ax1.annotate('Buy Exit', (row['updated_on'], row['cPrice']), textcoords='data', color='green')
                     if row['status'] == -1:
                         ax1.annotate('Buy Exit', (row['updated_on'], row['cPrice']), textcoords='data', color='red')
             if row['call'] == 2:
                 ax1.annotate('Sell', (row['inserted_on'], (row['price'])), textcoords='data')
-                if row['status'] !=0:
+                if row['status'] != 0:
                     if row['status'] == 1:
-                        plt.annotate('Sell Exit', (row['updated_on'], row['cPrice']), textcoords='data', color='green')
-                if row['status'] !=0:
+                        ax1.annotate('Sell Exit', (row['updated_on'], row['cPrice']), textcoords='data', color='green')
                     if row['status'] == -1:
-                        plt.annotate('Sell Exit', (row['updated_on'], row['cPrice']), textcoords='data', color='red')
-    # ax2.clear()
-    # ax3.clear()
-    # ax4.clear()
-    # ax2.plot(indVal['insert_on'], indVal['indicator3'])
-    # ax3.plot(indVal['insert_on'], indVal['indicator4'])
-    # ax4.plot(indVal['insert_on'], indVal['indicator5'])
+                        ax1.annotate('Sell Exit', (row['updated_on'], row['cPrice']), textcoords='data', color='red')
+    ax2.clear()
+    ax3.clear()
+    ax4.clear()
+    ax2.plot(indVal['insert_on'], indVal['indicator3'])
+    ax3.plot(indVal['insert_on'], indVal['indicator4'])
+    ax4.plot(indVal['insert_on'], indVal['indicator5'])
 fig.subplots_adjust(hspace=0)
 animate(5000)
 # ani = animation.FuncAnimation(fig, animate, interval=5000)
